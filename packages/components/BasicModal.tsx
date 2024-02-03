01e1200c067ab6137d535457cbca9278c23c2780
“YunChe05” <mikebarquilla8@gmail.com>
2024-02-03 15:49:00 +0800

    feat: Added the modal and the date

---
+++ packages/components/BasicModal.tsx
@@ -0,0 +1,37 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { FC, useState } from "react";
+
+type BasicModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  width?: number;
+  height?: number;
+};
+export const BasicModal: FC<BasicModalProps> = ({
+  isOpen,
+  onClose,
+  height = 500,
+  width = 500,
+  children,
+}) => {
+  return (
+    <div>
+      <Modal
+        open={isOpen}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[${width}px] h-[${height}px] bg-white border-2 border-solid border-black shadow-md p-4`}
+        >
+          {children as React.ReactElement}
+        </div>
+      </Modal>
+    </div>
+  );
+};
--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,40 +1,34 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout";
 import dynamic from "next/dynamic";
 import { DatePickerComponent } from "../../../packages/components/DatePickerComponent";
-import { TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { BasicModal } from "../../../packages/components/BasicModal";
 
 const PieChart = dynamic(() => import("./charts/pieChart"), {
-  loading: () => <p className="text-white">Chart is Preparing...</p>,
-});
-
-const GeoChart = dynamic(() => import("./charts/geoChart"), {
   loading: () => <p className="text-white">Chart is Preparing...</p>,
 });
 
 const BarChart = dynamic(() => import("./charts/barChart"), {
   loading: () => <p className="text-white">Chart is Preparing...</p>,
 });
-
-const AreaChart = dynamic(() => import("./charts/areaChart"), {
-  loading: () => <p className="text-white">Chart is Preparing...</p>,
-});
-
-const BubbleChart = dynamic(() => import("./charts/BubbleChart"), {
-  loading: () => <p className="text-white">Chart is Preparing...</p>,
-});
-
-const GanttChart = dynamic(() => import("./charts/ganttChart"), {
-  loading: () => <p className="text-white">Chart is Preparing...</p>,
-});
-
 export default function Analytics() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => {
+    setIsOpen(false);
+  };
   return (
     <>
       <Layout>
         <>
-          <h2 className="text-lg mb-2">Analytics</h2>
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg mb-2">Analytics</h2>
+            <Button onClick={handleOpen} variant="outlined">
+              Filter
+            </Button>
+          </div>
           <div className="grid grid-cols-12 gap-4">
             <div className="col-span-5 rounded-lg shadow-lg px-4 py-4 bg-gray-700">
               <PieChart />
@@ -45,6 +39,14 @@ export default function Analytics() {
           </div>
         </>
       </Layout>
+      <BasicModal isOpen={isOpen} onClose={handleClose}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Filters
+        </Typography>
+        <div className="flex items-center justify-center">
+          <DatePickerComponent />
+        </div>
+      </BasicModal>
     </>
   );
 }
